1af3ca749536562fa7d35e801609fc21bfba9e3f
Nikolai Nanev <niki4etooo@gmail.com>
2022-01-26 21:31:11 +0200

    added example planet object

--- public/js/planets.js
+++ public/js/planets.js
@@ -10,6 +10,18 @@ var planet = {
 	farFromSun: 0
 };
 
+planet.name = "Earth";
+planet.orbit = true;
+planet.radius = 10000;
+planet.population = 500;
+planet.life = true;
+planet.speed = 240;
+planet.farFromSun = 150000000;
+
+console.log(planet.name);
+console.log(planet.orbit);
+console.log(planet.radius);
+
 var displayPlanet = function(planets) {
 	for(var planet of planets){
 		console.log(planet);
